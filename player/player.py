383b0cb12711e962cf73d3d290358806c3e7158f
Ali Badawi <ali.badawi@learnet.se>
2025-10-06 13:02:32 +0200

    added some functions

--- player/player.py
+++ player/player.py
@@ -1,5 +1,7 @@
-class Player:
-    def __init__(self, name: str, balance: int = 0):
+from hand.hand import Hand
+
+class Player(Hand):
+    def __init__(self, name: str, balance: int = 0,):
         self.name = name
         self.balance = balance
         self.bet = 0
@@ -9,3 +11,16 @@ class Player:
             raise ValueError("Too broke")
         self.bet = amount
         self.balance -= amount
+
+    def win_bet(self):
+        self.balance += self.bet * 2
+        self.bet = 0
+
+    def lose_bet(self):
+        self.bet = 0
+
+    def show_balance(self):
+        return self.balance        
+
+    def return_bet(self):
+        pass
