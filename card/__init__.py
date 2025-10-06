e850d72d2195dedd428ff01cfe244d1edcc2cc43
Elvira <ajdar.elvira@hotmail.com>
2025-10-06 10:11:24 +0200

    card code

---
+++ card/__init__.py
@@ -0,0 +1,26 @@
+class Card:
+    suit = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    ranks = [ "2", "3","4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+    values = {str(i): i for i in range(2,11)}
+    values.update({"Jack":10, "Queen":10, "King": 10, "Ace":11})
+
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank 
+        self.value = Card.values[rank]
+
+    @property
+    def is_ace(self):
+        return self.rank == "Ace"
+
+    def __str__ (self):
+        return f"{self.rank} of {self.suit}"
+
+        
+
+
+
+
+
+
