707f704d3734955512cb44dcbdd14a918cc8f1d4
Ali Badawi <ali.badawi@learnet.se>
2025-10-08 10:07:50 +0200

    Player är klar!

--- player/player.py
+++ player/player.py
@@ -1,10 +1,12 @@
 from hand.hand import Hand
+from card.card import Card
 
-class Player(Hand):
-    def __init__(self, name: str, balance: int = 0,):
+class Player():
+    def __init__(self, name: str, balance: int = 1000):
         self.name = name
         self.balance = balance
         self.bet = 0
+        self.hand = Hand()
 
     def place_bet(self, amount: int):    
         if amount > self.balance:
@@ -13,14 +15,26 @@ class Player(Hand):
         self.balance -= amount
 
     def win_bet(self):
-        self.balance += self.bet * 2
+        self.balance += self.bet * 2 
+        self.bet = 0
+
+    def blackjack_win(self):
+        self.balance += self.bet * 2.5
         self.bet = 0
 
     def lose_bet(self):
         self.bet = 0
 
     def show_balance(self):
-        return self.balance        
+        return self.balance  
+
+    def hit(self, card):
+        self.hand.add_card(card)
+
+    def reset_hand(self):
+        self.hand = Hand()
+        self.bet = 0
 
     def return_bet(self):
-        pass
+        self.balance += self.bet
+        self.bet = 0
