f96095d096694b65b436bb4dac8ea8855b822261
Victoria <VictoriaHart3@gmail.com>
2025-10-13 12:19:38 +0200

    updated comments in test file

--- test_deck.py
+++ test_deck.py
@@ -3,20 +3,20 @@ from deck.deck import Deck
 
 # Test that deck starts with 52 cards
 def test_deck_starts_with_52():
-    deck = Deck()
-    assert deck.remaining_cards() == 52
+    deck = Deck()                           # create a deck of cards
+    assert deck.remaining_cards() == 52     # return how many cards are in the deck list
 
 # Test that drawing reduces the deck size by 1
 def test_drawing_reduceby1():
-    deck = Deck()
-    pre_draw = deck.remaining_cards()
-    deck.draw_card()
-    post_draw = deck.remaining_cards()
-    assert pre_draw - post_draw == 1
+    deck = Deck()                       # create a deck of cards
+    pre_draw = deck.remaining_cards()   # count the deck 
+    deck.draw_card()                    # draw the last card (top card) in the deck
+    post_draw = deck.remaining_cards()  # count the deck after one card was drawn
+    assert pre_draw - post_draw == 1    # test that the difference before/after draw is 1 (only one card taken)
 
 # Test that shuffle() shuffles the cards
 def test_shuffle():
-    deck = Deck()
+    deck = Deck()                       # create a deck of cards
     pre_shuffle = deck.deck.copy()
     deck.shuffle()
     post_shuffle = deck.deck
@@ -24,10 +24,9 @@ def test_shuffle():
 
 # Test that reset_deck() restores to 52 cards
 def test_reset_to52():
-    deck = Deck()
-    deck.reset_deck()
-    remaining = deck.remaining_cards()
-    assert remaining == 52
+    deck = Deck()                       # create a deck of cards
+    deck.reset_deck()                   # tömmer kortleklistan, återskapar kortleken och blandar den
+    assert deck.remaining_cards() == 52 # kolla att den nya kortleken har 52 kort
 
 
 if __name__ == "__main__":
